2d2928312ec9e9986fbb6cd2ec3c67aa6e76bfd4
Juan Espinosa <juancamiloespinosab@gmail.com>
2021-06-02 10:05:04 -0500

    Se agrega rutas base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './components/organisms/details/details.component';
+import { MainComponent } from './components/pages/main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details/:id', component: DetailsComponent, outlet: 'aside' },
+  { path: 'main', component: MainComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '**', redirectTo: 'main', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
